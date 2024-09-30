4ac2762a8c79faa843f5e4448ffb9d91029bc01d
Toshkootpochivka <karadakovg@gmail.com>
2024-09-30 13:45:54 +0200

    added blur effect to the pop up windows

--- frontend/src/components/popup/PopUpWindow.tsx
+++ frontend/src/components/popup/PopUpWindow.tsx
@@ -7,20 +7,22 @@ import "../../styles/popup.css";
 
 const PopUpWindow:React.FC<PPType> = ({isOpen, popUpName, inputName1, inputName2, inputPlaceHolder1, inputPlaceHolder2, btnText, className, closePopUp, submitPopUp}) => {
   return (
-    <div className={`${isOpen ? className : "hidden"} absolute w-[90%] h-1/2 bg-gray-950 flex flex-col items-center rounded-xl md:w-2/3 lg:w-1/3 `}>
-      <h1 className="mt-4 text-red-300 font-bold text-2xl">{popUpName}</h1>
-      <input name={inputName1} className="mt-6 text-white font-bold text-center rounded-xl bg-gray-800 w-[80%] h-1/6 focus:outline-none focus:ring-0 focus:border-transparent" placeholder={inputPlaceHolder1}/>
-      <input name={inputName2} className="mt-6 text-white font-bold text-center rounded-xl bg-gray-800 w-[80%] h-1/6 focus:outline-none focus:ring-0 focus:border-transparent" placeholder={inputPlaceHolder2}/>
+    <div className={`${isOpen ? className : "hidden"} absolute w-[90%] h-1/2 flex flex-col justify-center items-center md:w-2/3 lg:w-1/3`}>
+      <div className={`z-40 absolute w-full h-full bg-gray-950 flex flex-col items-center rounded-xl`}>
+        <h1 className="mt-4 text-red-300 font-bold text-2xl">{popUpName}</h1>
+        <input name={inputName1} className="mt-6 text-white font-bold text-center rounded-xl bg-gray-800 w-[80%] h-1/6 focus:outline-none focus:ring-0 focus:border-transparent" placeholder={inputPlaceHolder1}/>
+        <input name={inputName2} className="mt-6 text-white font-bold text-center rounded-xl bg-gray-800 w-[80%] h-1/6 focus:outline-none focus:ring-0 focus:border-transparent" placeholder={inputPlaceHolder2}/>
 
-      <p className="mt-10 text-red-300 font-bold text-xl">Specify Gender:</p>
-      <div className="w-1/2 h-1/6 flex justify-between items-center">
-        <RadioButton name="gender" text="male" className="w-1/2 mr-4"/>
-        <RadioButton name="gender" text="female" className="w-1/2 ml-4"/>
-      </div>
+        <p className="mt-10 text-red-300 font-bold text-xl">Specify Gender:</p>
+        <div className="w-1/2 h-1/6 flex justify-between items-center">
+          <RadioButton name="gender" text="male" className="w-1/2 mr-4"/>
+          <RadioButton name="gender" text="female" className="w-1/2 ml-4"/>
+        </div>
 
-      <div className="mt-12 px-6 flex justify-between w-full h-1/6">
-        <ButtonChat onClick={closePopUp} text="Cancel" className="w-1/3 h-2/3 border-none text-white font-bold rounded-lg bg-gray-600 lg:h-3/4"/>
-        <ButtonChat onClick={submitPopUp} text={btnText} className="w-1/3 h-2/3 border-none text-white font-bold rounded-lg bg-purple-500 lg:h-3/4"/>
+        <div className="mt-12 px-6 flex justify-between w-full h-1/6">
+          <ButtonChat onClick={closePopUp} text="Cancel" className="w-1/3 h-2/3 border-none text-white font-bold rounded-lg bg-gray-600 lg:h-3/4"/>
+          <ButtonChat onClick={submitPopUp} text={btnText} className="w-1/3 h-2/3 border-none text-white font-bold rounded-lg bg-purple-500 lg:h-3/4"/>
+        </div>
       </div>
     </div>
   )
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useState, React} from "react";
 import ButtonChat from "../components/ButtonChat";
 import PopUpWindow from "../components/popup/PopUpWindow";
 import Typewriter from "../components/Typewriter";
@@ -8,8 +8,14 @@ import "../styles/styles.css";
 function Home() {
   const [openCR, setOpenCR] = useState(false);
   const [openJR, setOpenJR] = useState(false);
+
+  const onToggle = (setOpen: React.Dispatch<React.SetStateAction<boolean>>) => {
+    setOpen(prev => !prev);
+  };
+
   return (
     <>
+      <div className={`${openCR ? "" : (openJR ? "" : "hidden")} absolute top-0 left-0 w-full h-full z-20 backdrop-blur`}></div>
       <div className="w-full h-full flex flex-col justify-center items-center max-lg:gap-y-10 lg:flex-row-reverse">
         <h1 className="p-3 mt-6 w-full text-white text-7xl text-center font-bold max-sm:text-4xl max-md:text-4xl md:text-5xl md:px-8">
           <Typewriter text="Welcome to the live chatroom, press Create room to create your own room which other people can join with code or press join room if you have a code for another room!" delay={50} />
@@ -17,12 +23,12 @@ function Home() {
 
         <div className="w-full h-1/2 flex flex-col justify-center items-center gap-y-10 lg:bg-black lg:h-full lg:w-1/3">
           <ButtonChat
-            onClick={() => setOpenCR(true)}
+            onClick={() => onToggle(setOpenCR)}
             text="Create room"
             className="w-2/3 h-1/6 rounded-xl bg-purple-500 text-xl text-white font-bold border-none lg:h-[10%]"
           />
           <ButtonChat
-            onClick={() => setOpenJR(true)}
+            onClick={() => onToggle(setOpenJR)}
             text="Join room"
             className="w-2/3 h-1/6 rounded-xl bg-purple-500 text-xl text-white font-bold border-none lg:h-[10%]"
           />
